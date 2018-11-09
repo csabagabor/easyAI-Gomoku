f18d92257e3d45ed57ea28bccc0336c5a6a493f9
csaba97 <csabagabor97@google.com>
2018-11-09 21:19:14 +0200

    Revert "unmake_move instead of deepcopy"
    This reverts commit 2e009093f4a675709c103204d11d0c5d59c5d8ba.

--- easyAI/games/AI_Player_Iterative_Deepening.py
+++ easyAI/games/AI_Player_Iterative_Deepening.py
@@ -5,6 +5,10 @@ from easyAI import TT
 
 
 class AI_Player_Iterative_Deepening:
+    """
+    Class for an AI player. This class must be initialized with an
+    AI algortihm, like ``AI_Player( Negamax(9) )``
+    """
 
     def __init__(self, timeout=5, name='AI_iterative'):
         self.name = name
@@ -15,9 +19,10 @@ class AI_Player_Iterative_Deepening:
     def ask_move(self, game):
         self.max_time = time.time() + self.timeout
         last = game.possible_moves()[0]
+        game_copy = deepcopy(game)
         for depth in range(1, 20):
             ai = Negamax_Iterative_Deepening(depth=depth, tt=self.tt)
-            result = ai(game, timeout=self.max_time)
+            result = ai(game_copy, timeout=self.max_time)
             if result != 9999:
                 last = result
             else:
