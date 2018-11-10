28afebdbf02423fbe10b5864b6a7d61399dc994a
csaba97 <csabagabor97@google.com>
2018-11-10 11:50:50 +0200

    Revert "test"
    This reverts commit 711b6d6c9bb1f968a9ddf7eb109d5cc93ad47e6f.

--- easyAI/games/AI_Player_Iterative_Deepening.py
+++ easyAI/games/AI_Player_Iterative_Deepening.py
@@ -20,16 +20,11 @@ class AI_Player_Iterative_Deepening:
         self.max_time = time.time() + self.timeout
         last = game.possible_moves()[0]
         game_copy = deepcopy(game)
-        for depth in range(1, 4):
+        for depth in range(1, 2):
             ai = Negamax_Iterative_Deepening(depth=depth, tt=self.tt)
-            move, alpha = ai(game_copy, timeout=self.max_time)
-            print move, alpha
-            if depth == 1 and alpha == 100:#can win with first move
-                return move
-            # if depth == 2 and alpha < -99:#certain lose if not immediate counter-attack
-            #     return move
-            if move != 9999:
-                last = move
+            result = ai(game_copy, timeout=self.max_time)
+            if result != 9999:
+                last = result
             else:
                 break
             print("depth:%d" % depth)
